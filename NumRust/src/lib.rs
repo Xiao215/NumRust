d8e53aae8765800120d767f9546a62c16c3a20d6
Xiao215 <xiaozhang20030215@gmail.com>
2023-03-07 15:56:20 -0500

    initialized a simple rust package for prime number

---
+++ NumRust/src/lib.rs
@@ -0,0 +1,32 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn primecounter(range_from: u64, range_til: u64) -> (u32, u32) {
+    /* Returns the number of found prime numbers between [range_from] and [range_til] """ */
+    let mut prime_count: u32 = 0;
+    let mut check_count: u32 = 0;
+    let _from: u64 = if range_from < 2 { 2 } else { range_from };
+    let mut prime_found: bool;
+
+    for num in _from..=range_til {
+        prime_found = false;
+        for divnum in 2..num {
+            check_count += 1;
+            if num % divnum == 0 {
+                prime_found = true;
+                break;
+            }
+        }
+        if !prime_found {
+            prime_count += 1;
+        }
+    }
+    return (prime_count, check_count);
+}
+
+/// Put the function in a Python module
+#[pymodule]
+fn my_rust_module(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(primecounter, m)?)?;
+    Ok(())
+}
