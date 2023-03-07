2b970b3bbb0f29371d1fe1b580d827686bc76ca6
Xiao215 <xiaozhang20030215@gmail.com>
2023-03-07 18:00:30 -0500

    strucutre set up

---
+++ NumRust/python/numrust/__init__.py
@@ -0,0 +1,6 @@
+from .numrust import *
+
+
+__doc__ = numrust.__doc__
+if hasattr(numrust, "__all__"):
+    __all__ = numrust.__all__
---
+++ NumRust/test.py
@@ -0,0 +1,5 @@
+import numrust
+
+primecount, eval_count = numrust.primecounter(
+    range_from=0, range_til=500)
+# returns 95 22279
